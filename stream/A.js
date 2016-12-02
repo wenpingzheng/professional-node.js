bdbde5d85f44bdace21f22a4497dfab78c2eafc9
richard.g <richard@richardgs-iMac.local>
2016-12-02 10:42:57 +0800

    submit

--- stream/A.js
+++
@@ -1,5 +0,0 @@
-setInterval(function () {
-    var random = Math.floor(Math.random() * 10);
-    console.log(random);
-}, 1000);
-
--- stream/stream-test.js
+++
@@ -1,8 +0,0 @@
-var fs = require("fs");
-var path = __dirname + "/hello.txt";
-
-fs.open(path, "r", function (error, fd) {
-    console.log(fd);
-});
-
-
