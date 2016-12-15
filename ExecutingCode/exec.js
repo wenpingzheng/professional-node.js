e5d066070b984df211a0ceda74d9eb9907e6ea7a
richardgong1987 <909253305@qq.com>
2016-12-15 23:06:59 +0800

    submit

--- ExecutingCode/exec.js
+++ ExecutingCode/exec.js
@@ -270,4 +270,77 @@ process.on("message", function(message) {
 
 
 
+// var vm = require("vm");
+// var code = "console.log(foo);";
+// foo = "Hello vm";
+// vm.runInNewContext(code);//error
+//
+
+
+// var vm = require("vm");
+// var code = "console.log(foo);";
+// var sandbox;
+// foo = "Hello vm";
+// sandbox = {
+//     console: console,
+//     foo: foo
+// };
+// vm.runInNewContext(code, sandbox);
+
+/********************************************************************
+ * Sandboxing Data
+ */
+// var vm = require("vm");
+// var code = "var bar = 1; console.log(foo); foo = 'Goodbye'";
+// var sandbox;
+// foo = "Hello vm";
+// sandbox = {
+//     console: console,
+//     foo: foo
+// };
+// vm.runInNewContext(code, sandbox);
+// console.log(foo);
+// console.log(sandbox.foo);
+// console.log(sandbox.bar);
+
+
+/**************
+ *vm.createContext([initSandbox])
+ */
+// var vm = require("vm");
+// var code = "var bar = 1; console.log(foo); foo = 'Goodbye'";
+// var context;
+// foo = "Hello vm";
+// context = vm.createContext({
+//     console: console,
+//     foo: foo
+// });
+// vm.runInContext(code, context);
+// console.log(foo);
+// console.log(context.foo);
+// console.log(context.bar);
+
+
+/*************
+ * createScript()
+ * script.runInThisContext()
+ * script.runInNewContext([sandbox])
+ * script.runInContext(context)
+ */
+
+// var vm = require("vm");
+// var script = vm.createScript("i++;", "example.vm");
+// var sandbox = {
+//     i: 0
+// }
+// for (var i = 0; i < 10; i++) {
+//     script.runInNewContext(sandbox);
+// }
+// console.log(sandbox);
+// displays {i: 10}
+
+
+
+
+
 
