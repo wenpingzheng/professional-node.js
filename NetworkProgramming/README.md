4253e001286dfee03543b1a680bb3a034629d3e8
richardgong1987 <909253305@qq.com>
2016-12-20 22:18:51 +0800

    submit

---
+++ NetworkProgramming/README.md
@@ -0,0 +1,4 @@
+#Network Programming
+###Transmission Control Protocol=>TCP
+###User Datagram Protocol=>UDP
+###Domain Name System=>DNS
---
+++ NetworkProgramming/protocal.js
@@ -0,0 +1,10 @@
+/**
+ * Created by richard.g on 20/12/2016.
+ */
+var net = require("net");
+var server = net.createServer({
+    allowHalfOpen: false
+}, function(socket) {
+// handle connection
+});
+
