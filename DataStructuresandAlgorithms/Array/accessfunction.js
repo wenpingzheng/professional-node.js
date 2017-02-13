ff84b4a38fcaddbeb46d94602cafea80a8c4feae
richard.g <richard@richardgs-iMac.local>
2017-02-13 09:59:06 +0800

    submit

---
+++ DataStructuresandAlgorithms/Array/accessfunction.js
@@ -0,0 +1,22 @@
+/**
+ * Created by richard on 2/13/17.
+ */
+
+/**
+ *1.Searching for a Value
+ *key word:  indexOf,lastIndexOf
+ */
+
+var names = ["aaa", "bbb", "ccc", "ddd", "eee","ccc"];
+var name = 'ccc';
+var firstPos = names.indexOf(name);
+console.log("First found " + name + " at position " + firstPos);
+
+var lastPos = names.lastIndexOf(name);
+
+console.log("Last found " + name + " at position " + lastPos);
+
+
+
+
+
