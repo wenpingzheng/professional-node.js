b617c9f52c7c48e5491ec4f866f2c025b4768737
richard.g <richard@richardgs-iMac.local>
2017-02-16 09:29:55 +0800

    submit

--- DataStructuresandAlgorithms/ch05.LinkedLists/LinkedList.js
+++ DataStructuresandAlgorithms/ch05.LinkedLists/LinkedList.js
@@ -76,3 +76,14 @@ class LinkedList {
         return currNode;
     }
 }
+
+
+var cities = new LinkedList();
+cities.insert("Conway", "head");
+cities.insert("Russellville", "Conway");
+cities.insert("Carlisle", "Russellville");
+cities.insert("Alma", "Carlisle");
+cities.display();
+console.log();
+cities.remove("Carlisle");
+cities.display();
